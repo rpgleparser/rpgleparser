ff26c0334a5c5eed2415d5965ce0664b86d7ad29
David Gregory <davidgregory084@gmail.com>
2015-02-21 16:52:33 +0000

    Get test source files for TestPGMInFolder from src/test/resources.

--- src/test/java/org/rpgleparser/integration/TestPGMInFolder.java
+++ src/test/java/org/rpgleparser/integration/TestPGMInFolder.java
@@ -11,8 +11,8 @@ import java.util.List;
 public class TestPGMInFolder {
 
     @Test
-    public void testSinglePgm() throws Exception {
-        String rpgsource = TestUtils.loadFile("./dataqueue.rpgle");
+    public void testOneRpgSource() throws Exception {
+        String rpgsource = TestUtils.loadFileByPath(getResourcePath("./dataqueue.rpgle"));
         rpgsource = TestUtils.pad280(rpgsource);
         //System.out.println(rpgsource);
         System.out.println("---------------------------------------");
@@ -21,48 +21,41 @@ public class TestPGMInFolder {
     }
 
     @Test
-    public void testAllFirstDies() throws Exception {
+    public void testAllRpgSources() throws Exception {
         List<String> errors = new ArrayList<String>();
-        File dir = new File("C:\\temp\\rpg\\pplmishl_all");
-
         List<String> files = new ArrayList<String>();
+        File dir = new File(getResourcePath("./"));
 
-        boolean started = false;
         for (File file : dir.listFiles()) {
-            if (file.getName().startsWith("PPR751G.MBR")) {
-                started = true;
-            }
-            if (file.getName().equals("CLR200.MBR")
-                    || file.getName().equals("NAR100C.MBR")
-                    || file.getName().equals("PPR525FR.MBR")
-
-                    || file.getName().equals("MSR129.MBR")
-                    || file.getName().equals("HORT401E.MBR")
-                    || file.getName().equals("HIR207D.MBR")) { //skip
-                continue;
-            }
-
-            if (!started)
-                continue;
-            if (file.isFile() && file.getName().endsWith(".MBR")) {
-
+            if (isRpgSourceFile(file)) {
                 String rpgsource = TestUtils.loadFile(file);
                 rpgsource = TestUtils.pad280(rpgsource);
                 System.out.print("\r\n" + file + ":");
                 List<CommonToken> tokenList = TestUtils.runX(rpgsource, errors);
                 files.add(file.getName());
-                if (errors.size() > 0) {
 
-                    //System.out.println(tokenList);
-                    //System.out.println(errors);
+                if (errors.size() > 0) {
+                    System.out.println(tokenList);
+                    System.out.println(errors);
                     break;
                 }
-
             }
         }
         for (String file : files) {
             System.out.println("\t" + file);
         }
+    }
+
+    private String getResourcePath(String resourcePath) {
+        return getClass().getResource(resourcePath).getFile();
+    }
+
+    private boolean isRpgSourceFile(File file) {
+        String fileName = file.getName().toUpperCase();
+        return file.isFile() &&
+                (fileName.endsWith(".MBR")
+                        || fileName.endsWith(".RPGLE")
+                        || fileName.endsWith(".SQLRPGLE"));
     }
 
 }
