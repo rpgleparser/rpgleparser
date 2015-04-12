78e6671d65afe6e2d2843388b8a8abfab1a7d9eb
ryaneberly <ryaneberly@gmail.com>
2015-04-11 21:09:57 -0400

    fix proc tests

--- src/test/java/org/rpgleparser/TestP.java
+++ src/test/java/org/rpgleparser/TestP.java
@@ -19,7 +19,8 @@ public class TestP {
                 "     P getThingAvailable...\r\n" +
                         "     P                 B\r\n" +
                         "     D                 PI                  like(typeQuantity)\r\n" +
-                        "     D  SomeNumber                         like(typeP50u)                 const\r\n";
+                        "     D  SomeNumber                         like(typeP50u)                 const\r\n"+
+                        "     P                 E\r\n" ;
         inputString = TestUtils.padSourceLines(inputString, false);
         List<String> errors = new ArrayList<String>();
         List<CommonToken> tokenList = TestUtils.getParsedTokens(inputString, errors);
@@ -31,8 +32,9 @@ public class TestP {
     public void testProcedureInterfaceWithContinuationName1() {
         String inputString =
                 "     P Function        B  \r\n" +
-                        "     D                 PI                  like(typeQuantity)\r\n" +
-                        "     D  SomeNumber                         like(typeP50u)                 const\r\n";
+                "     D                 PI                  like(typeQuantity)\r\n" +
+                "     D  SomeNumber                         like(typeP50u)                 const\r\n"+
+        		"     P                 E\r\n" ;
         inputString = TestUtils.padSourceLines(inputString, false);
         List<String> errors = new ArrayList<String>();
         List<CommonToken> tokenList = TestUtils.getParsedTokens(inputString, errors);
